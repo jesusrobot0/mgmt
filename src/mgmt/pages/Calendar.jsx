b1ce64646ad439e7693c4b53bafc902f4f326b7f
jesusrobot0 <jesusrobot9000@icloud.com>
2024-07-20 17:11:34 -0500

    feat: add big-calendar library and configure date localization

--- src/mgmt/pages/Calendar.jsx
+++ src/mgmt/pages/Calendar.jsx
@@ -1,9 +1,50 @@
+import { Calendar } from "react-big-calendar";
+import { addHours } from "date-fns";
 import { Navbar } from "../components";
+import { localizer, getMessagesES } from "../../helpers";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const events = [
+  {
+    title: "Cumpleaños",
+    notes: "Hay que comprar pastel",
+    start: new Date(),
+    end: addHours(new Date(), 1),
+    bgColor: "#fafafa",
+    user: {
+      _id: "123",
+      name: "Jesús Velasco",
+    },
+  },
+];
 
 export function CalendarPage() {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    console.log({ event, start, end, isSelected });
+
+    const style = {
+      backgroundColor: "#327cf7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      color: "white",
+    };
+
+    return { style };
+  };
+
   return (
-    <>
+    <div className="flex flex-col gap-5">
       <Navbar />
-    </>
+      <Calendar
+        culture="es"
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 78px)" }}
+        messages={getMessagesES()}
+        eventPropGetter={eventStyleGetter}
+      />
+    </div>
   );
 }
